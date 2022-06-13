775298700f808029d7259d971a69774f22845ef2
ErJump <fedelijunior@gmail.com>
2022-06-13 15:26:58 +0200

    comments add

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
+//Dichiaro e assegno le costanti attraverso l'input dell'utente
 const userName = prompt("Inserisci il tuo nome");
 const userSurname = prompt("Inserisci il tuo cognome");
 const userColor = prompt("Inserisci il tuo colore preferito");
+//Dichiaro e assegno il numero 22 
 const currentYear = 22; 
-
+//Richiamo lo span con l'id user-password e faccio scrivere all'interno dell'elemento html le variabili dichiarate precedentemente.
 document.getElementById("user-password").innerHTML = userName + userSurname + userColor + currentYear;
