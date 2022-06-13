5d7dcdbd0d497e00433549a0de44b821e16780ed
ErJump <fedelijunior@gmail.com>
2022-06-13 15:43:03 +0200

    Mod: const currentYear now is a string

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 const userName = prompt("Inserisci il tuo nome");
 const userSurname = prompt("Inserisci il tuo cognome");
 const userColor = prompt("Inserisci il tuo colore preferito");
-//Dichiaro e assegno il numero 22 
-const currentYear = 22; 
+//Dichiaro e assegno la stringa "22" 
+const currentYear = "22"; 
 //Richiamo lo span con l'id user-password e faccio scrivere all'interno dell'elemento html le variabili dichiarate precedentemente.
 document.getElementById("user-password").innerHTML = userName + userSurname + userColor + currentYear;
